e5bafe13c12f8b96f76aaf3e6be74e9ae39e7fd9
Johannes Thomsen <43598221+joha321j@users.noreply.github.com>
2021-10-26 11:25:20 +0200

    Add EntityNotFoundException.cs

---
+++ backend/backend/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backend.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string entityName, int id)
+            : base($"No {entityName} with id: {id} was found.")
+        {
+        }
+    }
+}
