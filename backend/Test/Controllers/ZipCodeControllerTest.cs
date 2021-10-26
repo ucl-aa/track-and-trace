39cc4096144633755444a7dc32a21b051d72650a
Johannes Thomsen <43598221+joha321j@users.noreply.github.com>
2021-10-26 11:28:51 +0200

    Add NotFound result to controller

---
+++ backend/Test/Controllers/ZipCodeControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using Backend.Controllers;
+using Backend.Exceptions;
+using Backend.Models;
+using Backend.Services;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Test.Controllers
+{
+    public class ZipCodeControllerTest
+    {
+        private readonly IZipCodeService _zipCodeService;
+        private readonly ZipCodeController _controller;
+
+        public ZipCodeControllerTest()
+        {
+            List<ZipCode> zipCodes = new List<ZipCode>
+            {
+                new ()
+                {
+                    City = "Aalborg",
+                    ZipCodeValue = "4562",
+                },
+                new ()
+                {
+                    City = "Skanderborg",
+                    ZipCodeValue = "3216",
+                },
+            };
+
+            _zipCodeService = A.Fake<IZipCodeService>();
+            A.CallTo(() => _zipCodeService.GetAsync(null)).Returns(zipCodes);
+            _controller = new ZipCodeController(_zipCodeService);
+        }
+
+        [Fact]
+        public void Should_beControllerBase()
+        {
+            _controller.Should().BeAssignableTo(typeof(ControllerBase));
+        }
+
+        [Fact]
+        public void Should_callService_when_gettingZipCodes()
+        {
+            _controller.Get(null);
+
+            A.CallTo(() => _zipCodeService.GetAsync(null)).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public async void Should_returnNotFoundActionAndLog_when_serviceThrowsNotFoundException()
+        {
+            int id = 654;
+            A.CallTo(() => _zipCodeService.GetAsync(null))
+                .Throws(new EntityNotFoundException(nameof(ZipCode), id));
+
+            ActionResult<IEnumerable<ZipCode>> returnAction = await _controller.Get(null);
+
+            returnAction.Result.Should().BeOfType(typeof(NotFoundObjectResult));
+        }
+    }
+}
---
+++ backend/backend/Controllers/ZipCodeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Backend.Exceptions;
+using Backend.Models;
+using Backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ZipCodeController : ControllerBase
+    {
+        private readonly IZipCodeService _zipCodeService;
+
+        public ZipCodeController(IZipCodeService zipCodeService)
+        {
+            _zipCodeService = zipCodeService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ZipCode>>> Get(int? id)
+        {
+            try
+            {
+                return Ok(await _zipCodeService.GetAsync(id));
+            }
+            catch (EntityNotFoundException exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+    }
+}
