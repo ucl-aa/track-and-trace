d8d895ae743c09bafe91260f5eef239ce69036fb
Core <43536688+kaar1498@users.noreply.github.com>
2021-11-25 23:39:17 +0100

    Update loadscript.js

--- loadscript.js
+++ loadscript.js
@@ -1,7 +1,25 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, group, sleep } from 'k6';
+
+ const url = 'https://aa.seen.wtf/';
+
+ export let Options = {
+  stages: [
+    { duration: '5m', target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
+    { duration: '10m', target: 60 }, // stay at 60 users for 10 minutes
+    { duration: '3m', target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
+    { duration: '2m', target: 100 }, // stay at 100 users for short amount of time (peak hour)
+    { duration: '3m', target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
+    { duration: '10m', target: 60 }, // continue at 60 for additional 10 minutes
+    { duration: '5m', target: 0 }, // ramp-down to 0 users
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
 
 export default function () {
-  http.get('https://test.https://aa.seen.wtf/.io');
-  sleep(1);
+  http.get(url);
+  sleep(3);
 }
