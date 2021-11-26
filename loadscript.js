a1a8700a2c2289b036c6fe6993c3ff1e8cbbbedc
Kaare Veggerby Sandbøl <kaar1498@edu.ucl.dk>
2021-11-26 13:15:01 +0100

    Change cron, lower failure threshold, change stages

--- loadscript.js
+++ loadscript.js
@@ -5,16 +5,16 @@ import { check, group, sleep } from 'k6';
 
  export const options = {
   stages: [
-    { duration: '5m', target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-    { duration: '10m', target: 60 }, // stay at 60 users for 10 minutes
-    { duration: '3m', target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-    { duration: '2m', target: 100 }, // stay at 100 users for short amount of time (peak hour)
-    { duration: '3m', target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-    { duration: '10m', target: 60 }, // continue at 60 for additional 10 minutes
+    { duration: '5m', target: 30 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
+    { duration: '5m', target: 30 }, // stay at 60 users for 10 minutes
+    { duration: '3m', target: 50 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
+    { duration: '2m', target: 50 }, // stay at 100 users for short amount of time (peak hour)
+    { duration: '3m', target: 30 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
+    { duration: '5m', target: 30 }, // continue at 60 for additional 10 minutes
     { duration: '5m', target: 0 }, // ramp-down to 0 users
   ],
   thresholds: {
-    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    http_req_duration: ['p(99)<10000'], // 99% of requests must complete below 10s
   },
 };
 
