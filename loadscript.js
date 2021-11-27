b9fd43a172116ebd0e03821c0e6e0efc005fef15
Core <43536688+kaar1498@users.noreply.github.com>
2021-11-27 10:45:37 +0100

    Update loadscript.js

--- loadscript.js
+++ loadscript.js
@@ -5,13 +5,13 @@ import { check, group, sleep } from 'k6';
 
  export const options = {
   stages: [
-    { duration: '5m', target: 30 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-    { duration: '5m', target: 30 }, // stay at 60 users for 10 minutes
+    { duration: '3m', target: 30 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
+    { duration: '3m', target: 30 }, // stay at 60 users for 10 minutes
     { duration: '3m', target: 50 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
     { duration: '2m', target: 50 }, // stay at 100 users for short amount of time (peak hour)
     { duration: '3m', target: 30 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-    { duration: '5m', target: 30 }, // continue at 60 for additional 10 minutes
-    { duration: '5m', target: 0 }, // ramp-down to 0 users
+    { duration: '3m', target: 30 }, // continue at 60 for additional 10 minutes
+    { duration: '3m', target: 0 }, // ramp-down to 0 users
   ],
   thresholds: {
     http_req_duration: ['p(99)<10000'], // 99% of requests must complete below 10s
@@ -20,17 +20,5 @@ import { check, group, sleep } from 'k6';
 
 
 export default function () {
-  http.get(url);
-  sleep(3);
-  http.get(url);
-  sleep(3);
-  http.get(url);
-  sleep(3);
-  http.get(url);
-  sleep(3);
-  http.get(url);
-  sleep(3);
-  http.get(url);
-  sleep(3);
   http.get(url);
 }
