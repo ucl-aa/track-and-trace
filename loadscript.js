2cad46a22d8d6cf4d7581d98d806620b1cc0756b
Core <43536688+kaar1498@users.noreply.github.com>
2021-12-04 09:45:12 +0100

    Update loadscript.js

--- loadscript.js
+++ loadscript.js
@@ -5,12 +5,12 @@ import { check, group, sleep } from 'k6';
 
  export const options = {
   stages: [
-    { duration: '3m', target: 30 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-    { duration: '3m', target: 30 }, // stay at 60 users for 10 minutes
-    { duration: '3m', target: 50 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-    { duration: '2m', target: 50 }, // stay at 100 users for short amount of time (peak hour)
-    { duration: '3m', target: 30 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-    { duration: '3m', target: 30 }, // continue at 60 for additional 10 minutes
+    { duration: '3m', target: 30 }, // simulate ramp-up of traffic from 1 to 30 users over 5 minutes.
+    { duration: '3m', target: 30 }, // stay at 30 users for 10 minutes
+    { duration: '3m', target: 50 }, // ramp-up to 50 users over 3 minutes (peak hour starts)
+    { duration: '2m', target: 50 }, // stay at 50 users for short amount of time (peak hour)
+    { duration: '3m', target: 30 }, // ramp-down to 30 users over 3 minutes (peak hour ends)
+    { duration: '3m', target: 30 }, // continue at 30 for additional 10 minutes
     { duration: '3m', target: 0 }, // ramp-down to 0 users
   ],
   thresholds: {
@@ -22,3 +22,4 @@ import { check, group, sleep } from 'k6';
 export default function () {
   http.get(url);
 }
+
